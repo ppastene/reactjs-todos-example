7582525992b7a307e8989a1c27c905a44730a099
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-06-19 23:04:32 -0400

    Project bootstrapping

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
