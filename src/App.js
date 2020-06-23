b197f287c46004f5c0f92f25ade0358ae348ad54
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-06-23 00:39:38 -0400

    Addition of Components in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+ 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
