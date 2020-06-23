68180f2703660cb1296e19bb463407a636e4cfb5
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-06-23 00:39:09 -0400

    Remove of Navigation component in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Navigation from './components/Navigation';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navigation />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
