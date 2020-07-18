0d5998d6cf28416e462784ff34dcea940ccb2785
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-07-18 00:56:57 -0400

    Add of navigation components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import AuthContextProvider from './contexts/AuthContext';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { CookiesProvider } from 'react-cookie';
+import ProtectedRoute from './components/ProtectedRoute';
  
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navigation />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      </div>
-    </Router>
+    <CookiesProvider>
+      <Router>
+        <div className="App">
+          <AuthContextProvider>
+            <Navigation />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+              <ProtectedRoute path="/dashboard" component={Dashboard} />
+              <ProtectedRoute path="/admin" component={Admin} />
+            </Switch>
+          </AuthContextProvider>
+        </div>
+      </Router>
+    </CookiesProvider>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navbar, NavDropdown, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../contexts/AuthContext';
 
-function Navigation () {
+export default function Navigation () {
+
+    const { cookies, logout } = useContext(AuthContext)
+
+    const isLoggedIn = (cookies && cookies.auth && cookies.auth.isLoggedIn)
+
     return (
         <div>
             <Navbar bg="light" expand="lg">
@@ -10,25 +16,16 @@ function Navigation () {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+                    <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                    <Nav.Link as={Link} to="/admin">Admin</Nav.Link>
                 </Nav>
                 <Nav>
-                    <Nav.Link as={Link} to="/register">Register</Nav.Link>
-                    <Nav.Link href="#login">Login</Nav.Link>
-                    <Nav.Link href="#logout">Logout</Nav.Link>
+                    {isLoggedIn? <Nav.Link onClick={logout}>Logout</Nav.Link>:
+                        (<><Nav.Link as={Link} to="/register">Register</Nav.Link><Nav.Link as={Link} to="/Login">Login</Nav.Link></>)
+                    }
                 </Nav>
-                
                 </Navbar.Collapse>
             </Navbar>
         </div>
     );
 }
-
-export default Navigation;
