7811ed1a53a7ab80a1fb9ffe10b569a0732c85ac
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-07-18 00:57:17 -0400

    Register component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,7 +7,7 @@ class Register extends React.Component {
     constructor() {
         super();
         this.state = { name: null, email:null, password:null };
-      }
+    }
 
     onChange = event => {
         this.setState({ [event.target.name]: event.target.value });
@@ -19,13 +19,17 @@ class Register extends React.Component {
         const user = { name: this.state.name, email: this.state.email, password: this.state.password }
 
         axios.post('http://localhost:4000/register', {name: this.state.name, email: this.state.email, password: this.state.password})
+            .then(res => res.data)
             .then(res => {
-                alert(res.message)
+                console.log(res)
+                alert(res)
             })
             .catch(err => {
-                alert("error:"+err)
+                console.log(err.response.data.message)
+                alert(err.response.data.message)
             })
     }
+
 
     render() {
         return (
@@ -36,16 +40,16 @@ class Register extends React.Component {
                         <Form onSubmit={this.handleSubmit}>
                         <Form.Group controlId="formRegisterUsername">
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control name="name" type="text" placeholder="Enter username" onChange={this.onChange} />
+                                <Form.Control name="name" type="text" placeholder="Enter username" onChange={this.onChange} required />
                             </Form.Group>
                             <Form.Group controlId="formRegisterEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control name="email" type="email" placeholder="Enter email" onChange={this.onChange} />
+                                <Form.Control name="email" type="email" placeholder="Enter email" onChange={this.onChange} required />
                                 <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
                             </Form.Group>
                             <Form.Group controlId="formRegisterPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control name="password" type="password" placeholder="Enter password" onChange={this.onChange} />
+                                <Form.Control name="password" type="password" placeholder="Enter password" onChange={this.onChange} required />
                             </Form.Group>
                             <Form.Group>
                             <Button variant="primary" type="submit">Submit</Button>
@@ -57,31 +61,5 @@ class Register extends React.Component {
           );
     }
 }
-
-/*function Register() {
-  return (
-    <div className="Register">
-        <h1>Register</h1>
-        <Container>
-            <Card>
-                <Form>
-                    <Form.Group controlId="formRegisterEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-                    </Form.Group>
-                    <Form.Group controlId="formRegisterPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="password" />
-                    </Form.Group>
-                    <Form.Group>
-                    <Button variant="primary" type="submit">Submit</Button>
-                    </Form.Group>
-                </Form>
-            </Card>
-        </Container>
-    </div>
-  );
-}*/
 
 export default Register;
